b0e4597cfc3e07ee475198afa77aab259e2100ab
ch1m3z13 <chimeo81@gmail.com>
2025-10-15 05:55:21 +0100

    fix async storage import

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@react-native-async-storage/async-storage'] = false
+    return config
+  },
+}
 
-module.exports = nextConfig
+export default nextConfig
